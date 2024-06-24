0e97c2f401af43e5fd8aee573922cf8f30f75913
njugus <kayvohnjuguna@gmail.com>
2024-06-23 19:37:22 -0700

    adds the delete request

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ const app = express();
 
 app.use(express.json());
 
-app.use("/products", userRoutes);
+// app.use("/products", userRoutes);
 
 app.use((req, res, next) => {
     console.log(`${req.method} ${req.url}`);
